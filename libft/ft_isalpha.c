388cf70c4dcfc535063e3a8a57f3592e859afe83
mirrion <mirrion@gmail.com>
2022-02-18 15:00:37 +0000

    isalpha OK

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isalpha ()
+int ft_isalpha (char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		if((str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z'))
+			return(0);
+		i++;
+	}
+	return(1);
+}
+
