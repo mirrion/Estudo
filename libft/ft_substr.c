6ada38cefed34d9d5d8d47d96f88672cda166ff9
mirrion <mirrion@gmail.com>
2022-04-09 20:41:49 +0100

    cozinhando cabeça

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rosantan <rosantan@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 19:01:00 by rosantan          #+#    #+#             */
-/*   Updated: 2022/04/09 17:21:01 by rosantan         ###   ########.fr       */
+/*   Updated: 2022/04/09 20:41:06 by rosantan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	int		i;
 	int		size;
 
-	if (s == NULL)
-		return (NULL);
-	size = 1 + ft_strlen(s);
-	substr = (char *) malloc(sizeof(char) * size);
+	size = 1;
+	if (s[start])
+		size = ft_strlen(s + start) + 1 * sizeof * substr;
+	//printf("\n%i", size);
+	substr = malloc(size);
 	if (substr == NULL)
 		return (NULL);
 	i = 0;
