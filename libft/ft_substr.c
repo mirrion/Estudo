bcaac3e4a4d868c5268820e78c99cf66b120c748
mirrion <mirrion@gmail.com>
2022-04-10 10:37:59 +0100

    Terminado Substr falta split

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rosantan <rosantan@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 19:01:00 by rosantan          #+#    #+#             */
-/*   Updated: 2022/04/09 20:41:06 by rosantan         ###   ########.fr       */
+/*   Updated: 2022/04/10 09:27:09 by rosantan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,19 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
-	int		i;
-	int		size;
+	size_t	s_len;
 
-	size = 1;
-	if (s[start])
-		size = ft_strlen(s + start) + 1 * sizeof * substr;
-	//printf("\n%i", size);
-	substr = malloc(size);
+	if (s == NULL)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start > s_len)
+		return (ft_strdup(""));
+	if (start + len > s_len)
+		len = s_len - start;
+	substr = malloc(sizeof * substr * (len + 1));
 	if (substr == NULL)
 		return (NULL);
-	i = 0;
-	if (ft_strlen(s) > start)
-		while (len-- && s[start] != '\0')
-			substr[i++] = s[start++];
-	substr[i] = '\0';
+	ft_memcpy(substr, s + start, len);
+	substr[len] = '\0';
 	return (substr);
 }
