441dd3761ed2c61ad3987ce7f12fbabbdc45f9c2
mirrion <mirrion@gmail.com>
2022-04-28 15:42:43 +0100

    Iniciando

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,14 +6,16 @@
 /*   By: rosantan <rosantan@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 12:11:02 by rosantan          #+#    #+#             */
-/*   Updated: 2022/04/27 12:23:59 by rosantan         ###   ########.fr       */
+/*   Updated: 2022/04/28 15:42:34 by rosantan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *str, ...);
+int	ft_printf(const char *str, ...)
+{
 
+}
 
 int	main(void)
 {
