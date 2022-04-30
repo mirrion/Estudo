5ce46e153fe0070d3e6f92cd305c7392862602c3
mirrion <mirrion@gmail.com>
2022-04-30 11:08:10 +0100

    Exemple VA

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,19 +6,41 @@
 /*   By: rosantan <rosantan@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 12:11:02 by rosantan          #+#    #+#             */
-/*   Updated: 2022/04/28 15:42:34 by rosantan         ###   ########.fr       */
+/*   Updated: 2022/04/30 10:42:42 by rosantan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
+#include <stdarg.h>
 
-int	ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...);
+
+void	testit(int i, ...)
 {
+	va_list	argptr;
+	int		n;
+	char	*s;
 
+	va_start(argptr, i);
+	if (i == 0)
+	{
+		n = va_arg(argptr, int);
+		printf("%d\n", n);
+	}
+	else
+	{
+		s = va_arg(argptr, char *);
+		printf("%s\n", s);
+	}
+
+	va_end(argptr);
 }
 
 int	main(void)
 {
+	testit(1, "R"); // 1st problem: 0xffffffff is not an int
+    testit(1, NULL);       // 2nd problem: NULL is not a char*
 	printf("TEstando");
 	return (0);
 }
